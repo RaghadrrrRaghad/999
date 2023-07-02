6dd398ad1e14648d788d230089159b10473c69bc
Raghad <rghdhnya@gmail.com>
2023-07-02 13:43:06 +0300

    volume method

--- 999.py
+++ 999.py
@@ -20,3 +20,6 @@ class Parallelepipede(Rectangle):
     def __init__(self, length, width, height):
         super().__init__(length, width)
         self.height = height
+
+    def volume(self):
+        return self.length * self.width * self.height
