327922b66afda4ed2e1287f175b5e4d7ffdf12e7
Raghad <rghdhnya@gmail.com>
2023-07-02 13:41:04 +0300

    display method

--- 999.py
+++ 999.py
@@ -8,3 +8,9 @@ class Rectangle:
 
     def area(self):
         return self.length * self.width
+
+    def display(self):
+        print(f"Length: {self.length}")
+        print(f"Width: {self.width}")
+        print(f"Perimeter: {self.perimeter()}")
+        print(f"Area: {self.area()}")
